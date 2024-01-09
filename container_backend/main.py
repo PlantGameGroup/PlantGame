3570ba86822f4379d83611410856c03194c00955
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-01-09 09:54:47 +0100

    added TODOs

--- container_backend/main.py
+++ container_backend/main.py
@@ -3,5 +3,12 @@ from rabbitmq.listeners import start_listeners
 def main():
     start_listeners()
 
+    # TODO:
+    # - RabbitMQ reading all queues
+    # - extracting message content
+    # - plantnet request preparation
+    # - extracting necessary information out of plantnet response
+    # - calling gameboard API
+
 if __name__ == '__main__':
     main()
