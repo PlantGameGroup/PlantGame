896547a562f8cabf07276ef58bdb1d26a84f5e11
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-01-20 22:25:10 +0100

    added validation handler

--- container_backend/main.py
+++ container_backend/main.py
@@ -1,5 +1,6 @@
 from rabbitmq.listeners import start_listeners
 from validation.user_guess_handler import process_user_guess
+
 def main():
     process_user_guess(1,1,"https://www.plantura.garden/wp-content/uploads/2018/05/Orchidee-Bl%C3%BCte.jpg", "test")
 
--- container_backend/validation/user_guess_handler.py
+++ container_backend/validation/user_guess_handler.py
@@ -1,9 +1,14 @@
 from plantnet_handling.request_handler import plantnet_api_call
+from rabbitmq.tellers import send_guess_result
 
 threshold_score = 0.4
 
 def process_user_guess(gameID, plantID, imageURI, guessedSpecies):
-    best_match, best_match_score = plantnet_api_call(imageURI)
+    best_match_name , best_match_score = plantnet_api_call(imageURI)
 
     if best_match_score > threshold_score:
-        print(f"The {best_match} has made the threshold.")
+        print(f"The {best_match_name} has made the threshold.")
+        success = True
+    else :
+        success = False
+    send_guess_result(gameID, plantID, requestID, best_match_name, success)
