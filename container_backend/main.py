4e6119cd67ce1dfaed53ecc614cecf003d0cf49e
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-01-20 22:31:02 +0100

    changed user_geuss callback

--- container_backend/main.py
+++ container_backend/main.py
@@ -2,9 +2,7 @@ from rabbitmq.listeners import start_listeners
 from validation.user_guess_handler import process_user_guess
 
 def main():
-    process_user_guess(1,1,"https://www.plantura.garden/wp-content/uploads/2018/05/Orchidee-Bl%C3%BCte.jpg", "test")
-
-    # start_listeners()
+    start_listeners()
 
     # TODO:
     # - RabbitMQ reading all queues
--- container_backend/rabbitmq/callbacks.py
+++ container_backend/rabbitmq/callbacks.py
@@ -1,6 +1,7 @@
 import pika
 import json
 import requests
+from validation.user_guess_handler import process_user_guess
 
 def callback_user_guesses(ch, method, properties, body):
     # This is the callback function that will be called when a message is received
@@ -10,29 +11,11 @@ def callback_user_guesses(ch, method, properties, body):
     # Extract individual headers
     # Add plantID header
     gameID = message_body.get('gameID')
+    plantID = message_body.get('plantID')
     imageURI = message_body.get('imageURI')
     guessedSpecies = message_body.get('guessedSpecies')
 
-    # Make the API call to Plantnet
-    plantnet_api_url = 'https://my-api.plantnet.org/v2/identify/all'
-    plantnet_api_params = {
-        'images': imageURI,
-        'include-related-images': 'false',
-        'no-reject': 'false',
-        'lang': 'en',
-        'api-key': '2b10XVwzvES8lf368OlEP2vG'
-    }
-
-    response = requests.get(plantnet_api_url, params=plantnet_api_params)
-
-    # Handle the API response as needed
-    if response.status_code == 200:
-        plantnet_data = response.json()
-        # Process plantnet_data as needed
-        print(f"GameID: {gameID}, Guessed Species: {guessedSpecies}, Plantnet Response: {plantnet_data}")
-    else:
-        print(f"Error in Plantnet API call. Status Code: {response.status_code}, Response Text: {response.text}")
-
+    process_user_guess(gameID, plantID, imageURI, guessedSpecies)
 
     # Acknowledge the message to RabbitMQ
     ch.basic_ack(delivery_tag=method.delivery_tag)
