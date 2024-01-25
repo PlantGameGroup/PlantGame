431731cbd919fa92ab10f2b0de8f095938830f1d
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-01-25 11:56:30 +0100

    removed connection retry loop

--- container_backend/rabbitmq/tellers.py
+++ container_backend/rabbitmq/tellers.py
@@ -1,30 +1,17 @@
 import pika
 import json
 
-MAX_RETRIES = 5
-RETRY_INTERVAL = 10
-
 channel = None
 
 def setup_rabbitmq_teller():
     global channel
-    retries = 0
-    while retries < MAX_RETRIES:
-        try:
-            connection = get_rabbitmq_connection()
-            # Create a channel
-            channel = connection.channel()
-            # Declare the queue if not exists
-            channel.queue_declare(queue='guess_results')
-            print("RabbitMQ setup successful!")
-            return
-        except pika.exceptions.AMQPConnectionError as e:
-            print(f"RabbitMQ connection failed. Retrying in {RETRY_INTERVAL} seconds...")
-            retries += 1
-            time.sleep(RETRY_INTERVAL)
 
-    print(f"Max retries reached. Unable to set up RabbitMQ.")
-    # Handle the failure case or raise an exception as needed
+    connection = get_rabbitmq_connection()
+    # Create a channel
+    channel = connection.channel()
+    # Declare the queue if not exists
+    channel.queue_declare(queue='guess_results')
+    print("RabbitMQ setup successful!")
 
 def get_rabbitmq_connection():
     # Check if the connection already exists
@@ -59,7 +46,7 @@ def send_guess_result(game_id, plant_id, request_id, result_latin_name, confiden
             routing_key='guess_results',
             body=json_payload
         )
-    except pika.exceptions.AMQPConnectionError as e:
+    except Exception as e:
         print("RabbitMQ connection lost. Reconnecting...")
         setup_rabbitmq_teller()
         channel.basic_publish(
